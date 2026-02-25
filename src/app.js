c5e3a1f162108f6343138f66bc81dc4ba1e03b9a
neambuzdar-cmyk <neambuzdar@gmail.com>
2026-02-25 13:47:54 +0000

    Fix CORS issue: add Netlify domain to allowed origins

--- src/app.js
+++ src/app.js
@@ -78,6 +78,7 @@ app.use(cors({
   origin: process.env.CLIENT_URL || 'http://localhost:3000',
   credentials: true
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
